1b270cf65c55957cc78d6a7426bf568c3a7a4762
Jonathan Zhang <Jonathan.Zhang111@gmail.com>
2020-10-23 00:41:00 -0400

    Fixed timestamps and refactored + reformatted code

--- src/Cow.java
+++ src/Cow.java
@@ -1,6 +1,6 @@
 
-public class Cow implements Comparable<Cow>{
-	
+public class Cow implements Comparable<Cow> {
+
 	int id;
 	int latestWeight;
 	int lowestWeight;
@@ -8,89 +8,95 @@ public class Cow implements Comparable<Cow>{
 	int totalMilk;
 	int numOfMilkings; // to compute avgMilk
 	int timestamp; // has to be the latest
-	
-	
-	
-	
+
 	public Cow(int id) {
 		this.id = id;
-		this.latestWeight=0;
-		this.lowestWeight=10000;
-		this.avgMilk=0;
-		this.totalMilk=0;
-		this.numOfMilkings=0; 
-		this.timestamp=100000;
+		this.latestWeight = 0;
+		this.lowestWeight = Integer.MAX_VALUE;
+		this.avgMilk = 0;
+		this.totalMilk = 0;
+		this.numOfMilkings = 0;
+		this.timestamp = 0;
 	}
-	
+
 	public int getTotalMilk() {
 		return totalMilk;
 	}
 
 	public void setTotalMilk(int totalMilk) {
 		this.totalMilk += totalMilk;
-		//System.out.println("Total Milk for "+this.id+" updated to "+this.totalMilk);
+		// System.out.println("Total Milk for "+this.id+" updated to "+this.totalMilk);
 
 	}
 
 	public Cow() {
 	}
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public int getLatestWeight() {
 		return latestWeight;
 	}
+
 	public void setLatestWeight(int latestWeight) {
 		this.latestWeight = latestWeight;
 	}
+
 	public int getLowestWeight() {
 		return lowestWeight;
 	}
+
 	public void setLowestWeight(int lowestWeight) {
 		this.lowestWeight = lowestWeight;
 	}
+
 	public int getAvgMilk() {
 		return avgMilk;
 	}
+
 	public void setAvgMilk() {
-		this.avgMilk = this.totalMilk/this.numOfMilkings;
-		//System.out.println("Average Milk for "+this.id+" updated to "+this.avgMilk);
+		this.avgMilk = this.totalMilk / this.numOfMilkings;
+		// System.out.println("Average Milk for "+this.id+" updated to "+this.avgMilk);
 	}
+
 	public int getNumOfMilkings() {
 		return numOfMilkings;
 	}
+
 	public void incNumOfMilkings() {
-		this.numOfMilkings=this.numOfMilkings+1;
-		//System.out.println("Number of Milkings for "+this.id+" updated to "+this.numOfMilkings);
+		this.numOfMilkings = this.numOfMilkings + 1;
+		// System.out.println("Number of Milkings for "+this.id+" updated to
+		// "+this.numOfMilkings);
 
 	}
+
 	public int getTimestamp() {
 		return timestamp;
 	}
+
 	public void setTimestamp(int timestamp) {
 		this.timestamp = timestamp;
 	}
 
-	
-
 	@Override
 	public String toString() {
-		return id + " " + latestWeight + " " + lowestWeight+ " " +avgMilk+"\n";
+		return id + " " + lowestWeight + " " + latestWeight + " " + avgMilk + "\n";
 	}
-	
+
 	@Override
-    public int compareTo(Cow a) { 
-    	if(this.getLowestWeight() - a.getLowestWeight()==0) {
-    		return this.getLatestWeight() - a.getLatestWeight();
-    	}else if(this.getLatestWeight() - a.getLatestWeight()==0) {
-    		return this.getAvgMilk() - a.getAvgMilk();
-    	}else
-    		return this.getLowestWeight() - a.getLowestWeight(); 
-    } 	
-	
-	
+	public int compareTo(Cow a) {
+		if (this.getLowestWeight() - a.getLowestWeight() == 0) {
+			return this.getLatestWeight() - a.getLatestWeight();
+		} else if (this.getLatestWeight() - a.getLatestWeight() == 0) {
+			return this.getAvgMilk() - a.getAvgMilk();
+		} else
+			return this.getLowestWeight() - a.getLowestWeight();
+	}
+
 }
--- src/Main.java
+++ src/Main.java
@@ -8,90 +8,67 @@ import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws NumberFormatException, IOException  {
-		 // BufferedReader inFile = new BufferedReader(new FileReader("data.txt"));
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// BufferedReader inFile = new BufferedReader(new FileReader("data.txt"));
 		Scanner scan = new Scanner(System.in);
 		String in = scan.nextLine();
-		 BufferedReader inFile = new BufferedReader(new FileReader(in));
+		scan.close();
+		BufferedReader inFile = new BufferedReader(new FileReader(in));
 
-		  String[] info;
-		  String nextLine;
-		  int id;
-		  String type;
-		  int val;
-		  int time;
-		  ArrayList <Integer> ids = new ArrayList<Integer>();
-		  HashMap<Integer, Cow> cows = new HashMap<Integer, Cow>();
-		  while((nextLine=inFile.readLine()) != null) 
-		  {
-			  if(nextLine.length()<5) {
-				  continue; // skips first line that has number of records
-			  }
-			  
-		      info  = nextLine.split(" ");
-		      id = Integer.parseInt(info[0]);
-		      type = info[1];
-		      val = Integer.parseInt(info[2]);
-		      time = Integer.parseInt(info[3]);
-		      //check if the cow is in hashset, if it is then update it
-		      //else make  a new object and update it.
-		      if(cows.containsKey(id)) { 
-		    	  if(type.equals("M")) {
+		String[] info;
+		String nextLine;
+		int id;
+		String type;
+		int val;
+		int time;
+		ArrayList<Integer> ids = new ArrayList<Integer>();
+		HashMap<Integer, Cow> cows = new HashMap<Integer, Cow>();
+		while ((nextLine = inFile.readLine()) != null) {
+			if (nextLine.length() < 5) {
+				continue; // skips first line that has number of records
+			}
 
-		    		  cows.get(id).incNumOfMilkings();
-		    		  cows.get(id).setTotalMilk(val);
-		    		  cows.get(id).setAvgMilk();
+			info = nextLine.split(" ");
+			id = Integer.parseInt(info[0]);
+			type = info[1];
+			val = Integer.parseInt(info[2]);
+			time = Integer.parseInt(info[3]);
+			// check if the cow is in hashset, if it is then update it
+			// else make a new object and update it.
+			if (!(cows.containsKey(id))) {
+				cows.put(id, new Cow(id));
+				ids.add(id);
+			}
+			if (type.equals("M")) {
 
-		    	  }
-		    	  if(type.equals("W")) {
-		    		  if(time<cows.get(id).getTimestamp()) {
-		    			  cows.get(id).setTimestamp(time);
-		    			  cows.get(id).setLatestWeight(val);
-		    		  }
-		    		  if(val<cows.get(id).getLowestWeight()) {
-		    			  cows.get(id).setLowestWeight(val);
-		    		  }
-		    	  }
-		    	  
-		      }else {
-		    	  cows.put(id, new Cow(id));
-		    	  ids.add(id);
-		    	  if(type.equals("M")) {
+				cows.get(id).incNumOfMilkings();
+				cows.get(id).setTotalMilk(val);
+				cows.get(id).setAvgMilk();
 
-		    		  cows.get(id).incNumOfMilkings();
-		    		  cows.get(id).setTotalMilk(val);
-		    		  cows.get(id).setAvgMilk();
+			} else if (type.equals("W")) {
+				if (time > cows.get(id).getTimestamp()) {
+					cows.get(id).setTimestamp(time);
+					cows.get(id).setLatestWeight(val);
+				}
+				if (val < cows.get(id).getLowestWeight()) {
+					cows.get(id).setLowestWeight(val);
+				}
+			}
+		}
+		inFile.close();
+		
+		ArrayList<Cow> list = new ArrayList<Cow>();
+		for (int i : ids) {
+			if (cows.get(i).getNumOfMilkings() == 0 || cows.get(i).getLatestWeight() == 0)
+				continue;
+			list.add(cows.get(i));
+		}
+		Collections.sort(list);
 
-		    	  }
-		    	  if(type.equals("W")) {
-		    		  if(time<cows.get(id).getTimestamp()) {
-		    			  cows.get(id).setTimestamp(time);
-		    			  cows.get(id).setLatestWeight(val);
-		    		  }
-		    		  if(val<cows.get(id).getLowestWeight()) {
-		    			  cows.get(id).setLowestWeight(val);
-		    		  }
-		    	  }
-		      }
+		for (Cow c : list) {
+			System.out.print(c);
+		}
 
-		  }
-		  ArrayList <Cow> list = new ArrayList<Cow>();
-		  for(int i : ids) {
-			  if(cows.get(i).getNumOfMilkings()==0 && cows.get(i).getLatestWeight()==0) continue;
-			  list.add(cows.get(i));
-		  }
-		  Collections.sort(list);
-		  
-		  for(Cow c : list) {
-			  System.out.print(c);
-		  }
-		  
-	
 	}
-	
- 
 
 }
-
-
-
